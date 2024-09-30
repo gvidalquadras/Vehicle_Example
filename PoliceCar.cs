8e954b9319a74b56e26e38916e4eeb0620a1acc5
Gloria <Alejo@GVQF>
2024-09-30 11:40:43 +0200

    Agregar archivos de proyecto.

---
+++ PoliceCar.cs
@@ -0,0 +1,94 @@
+﻿namespace Practice1
+{
+    class PoliceCar : Vehicle
+    {
+        //constant string as TypeOfVehicle wont change allong PoliceCar instances
+        private const string typeOfVehicle = "Police Car";
+        private bool isPatrolling; 
+        private bool isChasing; 
+        private SpeedRadar speedRadar;
+        private PoliceStation station;
+
+        public PoliceCar(string plate) : base(typeOfVehicle, plate)
+        {
+            isPatrolling = false;
+            speedRadar = new SpeedRadar();
+        }
+
+        public void UseRadar(Vehicle vehicle)
+        {
+            if (isPatrolling)
+            {
+                speedRadar.TriggerRadar(vehicle);
+                string meassurement = speedRadar.GetLastReading();
+                Console.WriteLine(WriteMessage($"Triggered radar. Result: {meassurement}"));
+                if (meassurement == "Catched above legal speed.")
+                {
+                    string plateMalo = vehicle.GetPlate();
+                    SendAlarm(plateMalo);
+                }
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage($"has no active radar."));
+            }
+        }
+
+        public bool IsPatrolling()
+        {
+            return isPatrolling;
+        }
+
+        public bool IsChasing()
+        {
+            return isChasing;
+        }
+
+        private void SendAlarm(string plate)
+        {
+
+
+
+        }
+
+        private void StartChasing()
+        {
+
+        }
+
+        public void StartPatrolling()
+        {
+            if (!isPatrolling)
+            {
+                isPatrolling = true;
+                Console.WriteLine(WriteMessage("started patrolling."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("is already patrolling."));
+            }
+        }
+
+        public void EndPatrolling()
+        {
+            if (isPatrolling)
+            {
+                isPatrolling = false;
+                Console.WriteLine(WriteMessage("stopped patrolling."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("was not patrolling."));
+            }
+        }
+
+        public void PrintRadarHistory()
+        {
+            Console.WriteLine(WriteMessage("Report radar speed history:"));
+            foreach (float speed in speedRadar.SpeedHistory)
+            {
+                Console.WriteLine(speed);
+            }
+        }
+    }
+}
---
+++ PoliceStation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice1
+{
+
+    class PoliceStation
+    {
+        public List<PoliceCar> PoliceCarRegister { get; private set; }
+        private string alarmPlate;
+        public PoliceStation()
+        {
+            PoliceCarRegister = new List<PoliceCar>();
+            alarmPlate = "";
+        }
+
+        private void ShareAlarm()
+        {
+
+        }
+
+        private void AddPoliceCar(PoliceCar policeCar)
+        {
+            PoliceCarRegister.Add(policeCar);
+
+        }
+
+        private void RemovePoliceCar(PoliceCar policeCar)
+        {
+            PoliceCarRegister.Remove(policeCar);
+
+        }
+
+    }
+}
+
