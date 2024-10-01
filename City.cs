1c764745d7bdafe9c9c25c930a56b0ab420392b9
Gloria <202203923@alu.comillas.edu>
2024-10-01 10:24:25 +0200

    Creacion de la clase ciudad

---
+++ City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class City
+    {
+        private PoliceStation PoliceStation { get; set; }
+        private List<Taxi> TaxiLicences;
+
+
+        public City (PoliceStation policeStation)
+        {
+            PoliceStation = policeStation;
+            TaxiLicences = new List<Taxi>();
+
+        }
+
+        public void GrantTaxiLicence(Taxi taxi)
+        {
+            TaxiLicences.Add(taxi);
+
+        }
+
+        public void RemoveTaxiLicence(Taxi taxi)
+        {
+            TaxiLicences.Remove(taxi);
+
+        }
+
+    }
+}
