ad6744d78949ea5e8e71ec1fc935c3503d6c4390
Gloria <202203923@alu.comillas.edu>
2024-10-01 10:22:03 +0200

    Se ha añadido la clase patinete

---
+++ Scooter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Scooter: Vehicle
+    {
+        private static string typeOfVehicle = "Scooter";
+        private float speed { get; set; }
+        public Scooter() : base(typeOfVehicle, null)
+        {
+            speed = 0f;
+        }
+
+    }
+}
