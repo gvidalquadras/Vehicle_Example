0ccbb17c6c6564134c1ad538b4335a10221fe94d
gvidalquadras <202203923@alu.comillas.edu>
2024-10-03 14:23:48 +0200

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -7,34 +7,33 @@
         {
             Taxi taxi1 = new Taxi("0001 AAA");
             Taxi taxi2 = new Taxi("0002 BBB");
-            PoliceCar policeCar1 = new PoliceCar("0001 CNP");
+            PoliceCar policeCar1 = new PoliceCar("0001 CNP", speedRadar);
             PoliceCar policeCar2 = new PoliceCar("0002 CNP");
+            City city = new City("Madrid");
+            PoliceStation station = new PoliceStation("001", city);
+            
 
             Console.WriteLine(taxi1.WriteMessage("Created"));
             Console.WriteLine(taxi2.WriteMessage("Created"));
             Console.WriteLine(policeCar1.WriteMessage("Created"));
             Console.WriteLine(policeCar2.WriteMessage("Created"));
+            Console.WriteLine(city.WriteMessage("Created"));
+            Console.WriteLine(station.WriteMessage("Created"));
 
-            policeCar1.StartPatrolling();
-            policeCar1.UseRadar(taxi1);
+            station.AddPoliceCar(policeCar1);
+            station.AddPoliceCar(policeCar2);
 
+            city.GrantTaxiLicence(taxi1);
+            city.GrantTaxiLicence(taxi2);
+
+            taxi1.StartRide();
             taxi2.StartRide();
-            policeCar2.UseRadar(taxi2);
+
             policeCar2.StartPatrolling();
-            policeCar2.UseRadar(taxi2);
-            taxi2.StopRide();
-            policeCar2.EndPatrolling();
-
-            taxi1.StartRide();
-            taxi1.StartRide();
+            policeCar2.UseRadar(taxi1);
             policeCar1.StartPatrolling();
-            policeCar1.UseRadar(taxi1);
-            taxi1.StopRide();
-            taxi1.StopRide();
-            policeCar1.EndPatrolling();
+            policeCar1.UseRadar(taxi2);
 
-            policeCar1.PrintRadarHistory();
-            policeCar2.PrintRadarHistory();
 
         }
     }
