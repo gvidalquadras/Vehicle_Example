479920fb246d7e6e3288f29e4ad4e631d73a31c2
Gloria <202203923@alu.comillas.edu>
2024-10-03 14:06:28 +0200

    Added a city name to identify it

--- City.cs
+++ City.cs
@@ -6,29 +6,42 @@ using System.Threading.Tasks;
 
 namespace Practice1
 {
-    internal class City
+    internal class City: IMessageWritter
     {
         private PoliceStation PoliceStation { get; set; }
         private List<Taxi> TaxiLicences;
+        private string CityName { get; set; }
 
 
-        public City (PoliceStation policeStation)
+        public City (PoliceStation policeStation, string cityName)
         {
             PoliceStation = policeStation;
             TaxiLicences = new List<Taxi>();
+            CityName = cityName;
 
         }
 
         public void GrantTaxiLicence(Taxi taxi)
         {
             TaxiLicences.Add(taxi);
-
+            Console.WriteLine(WriteMessage($"{taxi} was granted a Taxi Licence."));
         }
 
         public void RemoveTaxiLicence(Taxi taxi)
         {
             TaxiLicences.Remove(taxi);
+            Console.WriteLine(WriteMessage($"{taxi} had their Taxi Licence revoked."));
 
+        }
+
+        public override string ToString()
+        {
+            return $"City {CityName}";
+        }
+
+        public string WriteMessage(string message)
+        {
+            return $"{this}: {message}";
         }
 
     }
