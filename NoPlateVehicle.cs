193a8642fbc5879d0cb79e1576d8ac5f4c1d9199
Gloria <202203923@alu.comillas.edu>
2024-10-03 14:04:27 +0200

    New class for vehicles without a plate so we can have the scooter

---
+++ NoPlateVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    abstract class NoPlateVehicle: Vehicle
+    {
+        public NoPlateVehicle(string typeOfVehicle): base(typeOfVehicle) { }
+
+        public override string ToString() {
+            return $"{GetTypeOfVehicle()}";
+
+        }
+    }
+}
