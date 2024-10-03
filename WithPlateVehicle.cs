50fff49c3e1a6d51ae7230fdf6a586d7467c77da
Gloria <202203923@alu.comillas.edu>
2024-10-03 14:03:23 +0200

    New class for vehicles with plate

---
+++ WithPlateVehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class WithPlateVehicle : Vehicle
+    {
+        private string plate;
+
+        public WithPlateVehicle(string typeOfVehicle, string plate) : base(typeOfVehicle)
+        { this.plate = plate; }
+
+        public string GetPlate()
+        { return this.plate; }
+
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
+        }
+    }
+}
